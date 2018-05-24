7cd813b011b8929aac204dc2722859be6a306816
Hanbi Hans Choi <personabe1984@gmail.com>
2018-05-24 23:56:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -64,11 +64,10 @@ function placeOrder(cardNumber) {
   if(!cardNumber){
     return `Sorry, we don't have a credit card on file for you.`;
   }else {
-    let total = total();
+    var cartTotal = total();
     cart = [];
     return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`; 
   }
-  
 }
 
 
