26db19af76a3ec4653449e4c9f8350ee98827eed
Hanbi Hans Choi <personabe1984@gmail.com>
2018-05-24 23:50:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -61,7 +61,7 @@ function removeFromCart(item) {
 }
 
 function placeOrder(cardNumber) {
-  console.log(cardNumber);
+  
   if(!cardNumber){
     return `Sorry, we don't have a credit card on file for you.`;
   }else {
